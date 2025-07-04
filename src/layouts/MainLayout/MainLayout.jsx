0ca282599d7fd5327f9a3d2ecb4c914df03f5876
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-07-04 16:18:55 +0600

    hero animation added

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,6 +1,9 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import { useGSAP } from "@gsap/react";
 import { Outlet } from "react-router";
 // import Navbar from "../../shared/Navbar/Navbar";
-
+gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP);
 const MainLayout = () => {
   return (
     <div>
--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -3,7 +3,47 @@ import playStore from "../../../assets/hero/playstore.png";
 import greenBg from "../../../assets/hero/greenBg.png";
 import leftCircle from "../../../assets/hero/leftCircle.png";
 import rightCircle from "../../../assets/hero/rightCircle.png";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 const Hero = () => {
+  useGSAP(() => {
+    const title = SplitText.create("#title", { type: "words" });
+    gsap.from("#leftCircle", {
+      x: -50,
+      opacity: 0,
+      duration: 1,
+    });
+    gsap.from("#rightCircle", {
+      x: 50,
+      opacity: 0,
+      duration: 1,
+    });
+    gsap.from("#phone", {
+      y: -250,
+      opacity: 0,
+      duration: 1,
+      ease: "bounce.out",
+    });
+    gsap.from(title.words, {
+      y: 100,
+      opacity: 0,
+      duration: 1,
+      stagger: 0.1,
+    });
+    gsap.from("#pera", {
+      y: 100,
+      opacity: 0,
+      duration: 1.6,
+      stagger: 0.1,
+    });
+    gsap.from("#buttons button", {
+      y: 100,
+      opacity: 0,
+      duration: 2,
+      stagger: 0.2,
+    });
+  });
   return (
     <div className=" min-h-screen">
       <div className="flex justify-around gap-10  items-center flex-col lg:flex-row-reverse">
@@ -12,6 +52,7 @@ const Hero = () => {
             src={leftCircle}
             alt="left-circle"
             className="absolute md:left-0 left-10"
+            id="leftCircle"
           />
 
           <img
@@ -19,24 +60,35 @@ const Hero = () => {
             alt="greenBg"
             className="absolute top-20 left-15  w-sm"
           />
-          <img src={phone} className=" relative left-14 max-w-sm rounded-lg" />
+          <img
+            src={phone}
+            className=" relative left-14 max-w-sm rounded-lg"
+            id="phone"
+          />
           <img
             src={rightCircle}
             alt="right-circle"
             className="absolute -bottom-5 right-10 md:right-0 "
+            id="rightCircle"
           />
         </div>
         <div className="md:w-[500px]">
-          <h1 className="md:text-6xl text-5xl font-bold font-montHeavy lg:mt-20">
+          <h1
+            className="md:text-6xl text-5xl font-bold font-montHeavy lg:mt-20"
+            id="title"
+          >
             Wanna Own <span className="text-[#06582C]">Real Estate </span>With
             Just <br />{" "}
             <span className="md:text-8xl text-lime-500">500 Taka?</span>
           </h1>
-          <p className="py-6 font-montLight font-bold text-xl">
+          <p className="py-6 font-montLight font-bold text-xl" id="pera">
             Start investing in land, flats & resorts. Legally verified.
             Blockchain secured. Earn rental income & grow your wealth.
           </p>
-          <div className="flex flex-col md:flex-row lg:flex-col  gap-5">
+          <div
+            className="flex flex-col md:flex-row lg:flex-col  gap-5"
+            id="buttons"
+          >
             <button className="btn bg-[#06582C] text-white border-0 w-56">
               Get Started
             </button>
