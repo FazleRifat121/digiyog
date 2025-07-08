f455c68a8b959ab7673277b3ea2b259a97c8cd28
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-07-09 02:11:16 +0600

    faqhalf img first layer done

--- src/pages/Home/ClientReviews/FaqHalf.jsx
+++ src/pages/Home/ClientReviews/FaqHalf.jsx
@@ -1,12 +1,16 @@
 import circle from "../../../assets/faq/circle.png";
-
+import ellipse from "../../../assets/faq/Ellipse 34.png";
 const FaqHalf = () => {
   return (
     <div className="bg-green-900 min-h-screen flex items-center">
-      <div className="container mx-auto p-4">
-        <div className="flex items-center justify-around flex-col lg:flex-row-reverse gap-5">
-          <img src={circle} className="w-[300px]" />
-          <div className="lg:w-[500px] w-auto">
+      <div className="container mx-auto p-4 lg:mt-32">
+        <div className="flex items-center justify-center flex-col lg:flex-row-reverse gap-5 relative">
+          <img
+            src={ellipse}
+            className="absolute left-5 -top-8 w-[240px] md:left-44 md:-top-7  lg:w-auto lg:left-[550px] xl:left-[670px] 2xl:left-[800px] "
+          />
+          <img src={circle} className="w-[300px] lg:w-auto" />
+          <div className="lg:w-[500px] w-auto ">
             <p className="py-6 text-white font-montRegularg">
               Your investment shares are digitally secured on the blockchain.
               That means no one—not even us—can change or manipulate your
